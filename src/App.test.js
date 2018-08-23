42018574fb86e3be02ac573114fade8f3a47216c
Hendrik <hendrik@reinvent.nl>
2018-08-23 19:09:47 +0200

    added some very basic testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
 import App from './App';
+import { getRegexForFieldName, returnInput, customTranslate } from './services';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('mocks input', () => {
+  expect(returnInput('test')).to.equal('test');
+});
+
+it('gets regex for field', () => {
+  expect(getRegexForFieldName('telephone2').test('0987654321')).to.equal(true);
+});
+
+it('mocks translate', () => {
+  expect(customTranslate('address_import_schema_select')).to.equal('Map your fields');
+  expect(customTranslate('')).to.equal('');
 });
